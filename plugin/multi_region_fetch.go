12fdf3ef648e34684821bf1295f4e69e28f2fd34
kai <kai@turbot.com>
2021-01-29 15:43:13 +0000

    Rename *ForPartitions to *ForEach. GetForEach fails if multiple resaults are retrieved

--- plugin/multi_region_fetch.go
+++ plugin/multi_region_fetch.go
@@ -7,30 +7,30 @@ import (
 	"sync"
 )
 
-// ListForPartitions :: execute the provided list call for a set of partitions (e.g. region).
-// The partition is define by a parameter map containing required partition details (e.g. region name)
-func ListForPartitions(ctx context.Context, queryData *QueryData, hydrateData *HydrateData, listCall HydrateFunc, partitionParamsList []map[string]string) (interface{}, error) {
+// ListForEach :: execute the provided list call for each of a set of parameterMaps
+// intended for use with multi-region list calls
+func ListForEach(ctx context.Context, queryData *QueryData, hydrateData *HydrateData, listCall HydrateFunc, parameterMaps []map[string]string) (interface{}, error) {
 
-	log.Printf("[WARN] ListForPartitions, params: %v", partitionParamsList)
-	if len(partitionParamsList) == 0 {
-		return nil, fmt.Errorf("no input parameters passed to ExecListForEachParam")
+	log.Printf("[WARN] ListForEach, params: %v", parameterMaps)
+	if len(parameterMaps) == 0 {
+		return nil, fmt.Errorf("no input parameters passed to ListForEach")
 	}
-	if len(partitionParamsList) == 1 {
-		hydrateData.Params = partitionParamsList[0]
+	if len(parameterMaps) == 1 {
+		hydrateData.Params = parameterMaps[0]
 		return listCall(ctx, queryData, hydrateData)
 	}
 
 	var wg sync.WaitGroup
-	errorChan := make(chan error, len(partitionParamsList))
+	errorChan := make(chan error, len(parameterMaps))
 	var errors []error
-	for _, partitionParams := range partitionParamsList {
+	for _, parameterMap := range parameterMaps {
 		// clone hydrate data and set params
 		hd := hydrateData.Clone()
-		hd.Params = partitionParams
+		hd.Params = parameterMap
 		wg.Add(1)
 
 		go func() {
-			log.Printf("[WARN] run list with params: %v", partitionParams)
+			log.Printf("[WARN] run list with params: %v", parameterMap)
 			_, err := listCall(ctx, queryData, hd)
 			if err != nil {
 				errorChan <- err
@@ -57,22 +57,24 @@ func ListForPartitions(ctx context.Context, queryData *QueryData, hydrateData *H
 	}
 }
 
-// GetForPartitions :: execute the provided get call for a set of partitions (e.g. region).
-// The partition is define by a parameter map containing required partition details (e.g. region name)
-func GetForPartitions(ctx context.Context, queryData *QueryData, hydrateData *HydrateData, listCall HydrateFunc, partitionParamsList []map[string]string) (interface{}, error) {
+// GetForEach :: execute the provided get call for each of a set of parameterMaps
+// intended for use with multi-region list calls
+func GetForEach(ctx context.Context, queryData *QueryData, hydrateData *HydrateData, getCall HydrateFunc, partitionParamsList []map[string]string) (interface{}, error) {
 
-	log.Printf("[WARN] ListForPartitions, params: %v", partitionParamsList)
+	log.Printf("[WARN] ListForEach, params: %v", partitionParamsList)
 	if len(partitionParamsList) == 0 {
 		return nil, fmt.Errorf("no input parameters passed to ExecListForEachParam")
 	}
 	if len(partitionParamsList) == 1 {
 		hydrateData.Params = partitionParamsList[0]
-		return listCall(ctx, queryData, hydrateData)
+		return getCall(ctx, queryData, hydrateData)
 	}
 
 	var wg sync.WaitGroup
 	errorChan := make(chan error, len(partitionParamsList))
 	var errors []error
+	resultsChan := make(chan interface{}, len(partitionParamsList))
+	var results []interface{}
 	for _, partitionParams := range partitionParamsList {
 		// clone hydrate data and set params
 		hd := hydrateData.Clone()
@@ -81,9 +83,11 @@ func GetForPartitions(ctx context.Context, queryData *QueryData, hydrateData *Hy
 
 		go func() {
 			log.Printf("[WARN] run list with params: %v", partitionParams)
-			_, err := listCall(ctx, queryData, hd)
+			result, err := getCall(ctx, queryData, hd)
 			if err != nil {
 				errorChan <- err
+			} else {
+				resultsChan <- result
 			}
 			wg.Done()
 		}()
@@ -100,9 +104,22 @@ func GetForPartitions(ctx context.Context, queryData *QueryData, hydrateData *Hy
 		select {
 		case err := <-errorChan:
 			errors = append(errors, err)
+		case result := <-resultsChan:
+			if result != nil {
+				results = append(results, result)
+			}
 		case <-doneChan:
 			err := buildSingleError(errors)
-			return nil, err
+			// if we have more than one result, this means the key is not globally unique - an error
+			if len(results) > 1 {
+				return nil, fmt.Errorf("get call returned %d results - the key column is not globally unique", len(results))
+			}
+			var result interface{}
+			if len(results) == 1 {
+				result = results[0]
+			}
+
+			return result, err
 		}
 	}
 }
