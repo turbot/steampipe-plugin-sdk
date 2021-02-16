415e872ccf04f9d2fd8d7ddcf2b4514395f28502
kaidaguerre <kai@turbot.com>
2021-02-16 17:50:06 +0000

    Provide dependency mechanism to allow steampipe to know if a plugin uses a newer sdk version. Closes #25 (#33)

---
+++ version/version.go
@@ -0,0 +1,33 @@
+package version
+
+import (
+	"fmt"
+
+	version "github.com/hashicorp/go-version"
+)
+
+// The main version number that is being run at the moment.
+var Version = "0.2.0"
+
+// A pre-release marker for the version. If this is "" (empty string)
+// then it means that it is a final release. Otherwise, this is a pre-release
+// such as "dev" (in development), "beta", "rc1", etc.
+var Prerelease = "beta.0"
+
+// SemVer is an instance of version.Version. This has the secondary
+// benefit of verifying during tests and init time that our version is a
+// proper semantic version, which should always be the case.
+var SemVer *version.Version
+
+func init() {
+	str := Version
+	if Prerelease != "" {
+		str = fmt.Sprintf("%s-%s", Version, Prerelease)
+	}
+	SemVer = version.Must(version.NewVersion(str))
+}
+
+// String returns the complete version string, including prerelease
+func String() string {
+	return SemVer.String()
+}
