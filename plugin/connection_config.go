cf47a905c5d85b18879edb26caa2d95136ea2fa9
kai <kai@turbot.com>
2021-03-18 13:38:15 +0000

    Improve error message output for hcl diagnostics - update DiagsToError. Closes #72

--- plugin/connection_config.go
+++ plugin/connection_config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hcldec"
@@ -60,11 +61,11 @@ func (c *ConnectionConfigSchema) Parse(configString string) (config interface{},
 
 	file, diags := parser.ParseHCL([]byte(configString), "/")
 	if diags.HasErrors() {
-		return nil, DiagsToError("failed to parse connection config", diags)
+		return nil, DiagsToError("Failed to parse connection config", diags)
 	}
 	value, diags := hcldec.Decode(file.Body, spec, nil)
 	if diags.HasErrors() {
-		return nil, DiagsToError("failed to parse connection config", diags)
+		return nil, DiagsToError("Failed to parse connection config", diags)
 	}
 
 	// decode into the provided struct
@@ -82,20 +83,33 @@ func DiagsToError(prefix string, diags hcl.Diagnostics) error {
 	if !diags.HasErrors() {
 		return nil
 	}
+	errorStrings := []string{fmt.Sprintf("%s", prefix)}
+	// store list of messages (without the range) and use for deduping (we may get the same message for multiple ranges)
+	errorMessages := []string{}
 	for _, diag := range diags {
 		if diag.Severity == hcl.DiagError {
-			errString := fmt.Sprintf("%s", diag.Summary)
+			errorString := fmt.Sprintf("%s", diag.Summary)
 			if diag.Detail != "" {
-				errString += fmt.Sprintf(": %s", diag.Detail)
+				errorString += fmt.Sprintf(": %s", diag.Detail)
 			}
-			if prefix != "" {
-				errString = fmt.Sprintf("%s: %s", prefix, errString)
+
+			if !helpers.StringSliceContains(errorMessages, errorString) {
+				errorMessages = append(errorMessages, errorString)
+				// now add in the subject and add to the output array
+				if diag.Subject != nil {
+					errorString += fmt.Sprintf("\n(%s)", diag.Subject.String())
+				}
+				errorStrings = append(errorStrings, errorString)
+
 			}
-			if diag.Context != nil {
-				errString += fmt.Sprintf(" (%s) ", diag.Context.String())
-			}
-			return errors.New(errString)
 		}
+	}
+	if len(errorStrings) > 0 {
+		errorString := strings.Join(errorStrings, "\n")
+		if len(errorStrings) > 1 {
+			errorString += "\n"
+		}
+		return errors.New(errorString)
 	}
 	return diags.Errs()[0]
 }
