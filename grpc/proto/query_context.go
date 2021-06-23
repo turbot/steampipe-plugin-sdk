290dd753259e672d252d5ae78cf1cb1b3c728016
kai <kai@turbot.com>
2021-06-23 17:05:42 +0100

    comment

--- grpc/proto/query_context.go
+++ grpc/proto/query_context.go
@@ -1,5 +1,6 @@
 package proto
 
+// NewQueryContext creates a proto.QueryContext from provided columns, qualMap, and if non-nul, the limit
 func NewQueryContext(columns []string, qualMap map[string]*Quals, limit int64) *QueryContext {
 	var queryContext = &QueryContext{
 		Columns: columns,
