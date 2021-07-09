b09bfbf287689a5419c041b5f89d9439c3486ee2
kai <kai@turbot.com>
2021-07-09 18:01:27 +0100

    v1.3.0. Closes #146

--- version/version.go
+++ version/version.go
@@ -4,5 +4,5 @@ package version
 //// Update this if breaking changes are made to the protobuf spec (for example column types)
 var ProtocolVersion int64 = 20210701
 
-// Version is the monotonic version number reflecting the current release.
-var Version = "1.3"
+// Version is the semantic version number reflecting the current release.
+var Version = "1.3.0"
