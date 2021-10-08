c78be8993cc04b6f78ac6546d358bf0fcc623ebb
kai <kai@turbot.com>
2021-10-08 11:24:54 +0100

    initial

--- plugin/key_column_slice.go
+++ plugin/key_column_slice.go
@@ -50,6 +50,15 @@ func (k KeyColumnSlice) SingleEqualsQual() *KeyColumn {
 	return nil
 }
 
+func (k KeyColumnSlice) IsAnyOf() bool {
+	for _, kc := range k {
+		if kc.Require != AnyOf {
+			return false
+		}
+	}
+	return true
+}
+
 // Validate validates all child columns
 func (k KeyColumnSlice) Validate() []string {
 	var res []string
