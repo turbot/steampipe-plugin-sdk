24a6f9df9c0da30ffbe4ac14960188a904d2703f
kai <kai@turbot.com>
2021-11-05 18:02:37 +0000

    test

--- cache/query_cache.go
+++ cache/query_cache.go
@@ -78,9 +78,11 @@ func (c *QueryCache) Set(table string, qualMap map[string]*proto.Quals, columns 
 		indexBucket.Append(&IndexItem{columns, resultKey, limit})
 	} else {
 		// create new index bucket
-		indexBucket = newIndexBucket().Append(NewIndexItem(columns, resultKey, limit))
+		indexBucket = newIndexBucket().Append(NewIndexItem([]string{}, "", 0))
 	}
-	return c.cache.SetWithTTL(indexBucketKey, newIndexBucket().Append(NewIndexItem(columns, resultKey, limit)), 1, ttl)
+
+	indexBucket = newIndexBucket().Append(NewIndexItem([]string{}, "", 0))
+	return c.cache.SetWithTTL(indexBucketKey, indexBucket, 1, ttl)
 }
 
 func (c *QueryCache) Get(table string, qualMap map[string]*proto.Quals, columns []string, limit, ttlSeconds int64) *QueryCacheResult {
