6c03aeeb72979a5d173a1de64e05e548f7f114b1
kaidaguerre <kai@turbot.com>
2022-02-01 16:50:51 +0000

    Add steampipe_connection column to all tables. Closes #246

--- plugin/table_schema.go
+++ plugin/table_schema.go
@@ -1,23 +1,39 @@
 package plugin
 
 import (
+	"fmt"
+
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 )
 
-func (t Table) GetSchema() *proto.TableSchema {
+const ContextColumnName = "_ctx"
+
+func (t Table) GetSchema() (*proto.TableSchema, error) {
 	schema := &proto.TableSchema{
-		Columns:     make([]*proto.ColumnDefinition, len(t.Columns)),
+		Columns:     make([]*proto.ColumnDefinition, len(t.Columns)+1),
 		Description: t.Description,
 	}
 
+	// NOTE: we add a column "_ctx" to all tables.
+	// This is therefore a reserved column name
 	// column schema
 	for i, column := range t.Columns {
+		if column.Name == ContextColumnName {
+			return nil, fmt.Errorf("column '%s' is reserved and may not be used within a plugin schema", ContextColumnName)
+		}
 		schema.Columns[i] = &proto.ColumnDefinition{
 			Name:        column.Name,
 			Type:        column.Type,
 			Description: column.Description,
 		}
 	}
+	// add _ctx column
+	schema.Columns[len(t.Columns)] = &proto.ColumnDefinition{
+		Name:        ContextColumnName,
+		Type:        proto.ColumnType_JSON,
+		Description: "context data",
+	}
+
 	// key columns
 	if t.Get != nil && len(t.Get.KeyColumns) > 0 {
 		schema.GetCallKeyColumnList = t.Get.KeyColumns.ToProtobuf()
@@ -28,5 +44,5 @@ func (t Table) GetSchema() *proto.TableSchema {
 		}
 	}
 
-	return schema
+	return schema, nil
 }
