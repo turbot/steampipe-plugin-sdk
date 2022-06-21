af4b606b14268ca1d83c499383ab15f90cdf9f75
kai <kai@turbot.com>
2022-06-21 13:24:03 +0100

    Remove unused env var. #343

--- logging/constants.go
+++ logging/constants.go
@@ -3,10 +3,9 @@ package logging
 // environment variable defining the steampipe log level
 
 const (
-	LegacyProfileEnvVar = "SP_PROFILE"
-	LogLevelEnvVar      = "STEAMPIPE_LOG_LEVEL"
-	ProfileEnvVar       = "STEAMPIPE_PROFILE"
-	defaultLogLevel     = "WARN"
+	LogLevelEnvVar  = "STEAMPIPE_LOG_LEVEL"
+	ProfileEnvVar   = "STEAMPIPE_PROFILE"
+	defaultLogLevel = "WARN"
 )
 
 var LegacyLogLevelEnvVars = []string{"SP_LOG", "STEAMPIPE_LOG"}
