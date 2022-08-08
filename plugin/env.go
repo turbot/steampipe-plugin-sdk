ab83bfc127deec3e4055ca3a68638e8d23d1ecf2
kaidaguerre <kai@turbot.com>
2022-08-08 11:52:03 +0100

    Remove max concurrent row semaphore. Closes #369

--- plugin/env.go
+++ plugin/env.go
@@ -9,11 +9,9 @@ import (
 
 const (
 	envMaxConcurrentConnection      = "STEAMPIPE_MAX_CONCURRENT_CONNECTIONS"
-	envMaxConcurrentRow             = "STEAMPIPE_MAX_CONCURRENT_ROWS"
 	envMaxMemoryMb                  = "STEAMPIPE_MAX_MEMORY_MB"
 	envFreeMemInterval              = "STEAMPIPE_FREE_MEM_INTERVAL"
 	defaultMaxConcurrentConnections = 25            // default to 25 concurrent connections
-	defaultMaxConcurrentRows        = math.MaxInt64 // default to no row limit
 	defaultMaxMemoryMb              = math.MaxInt64 // default to no memory limit
 	defaultFreeMemInterval          = 100           // default to freeing memory every 100 rows
 )
@@ -25,17 +23,6 @@ func getMaxConcurrentConnections() int {
 	}
 	log.Printf("[INFO] Setting max concurrent connections to %d", maxConcurrentConnections)
 	return maxConcurrentConnections
-}
-
-func getMaxConcurrentRows() int {
-	maxConcurrentRows, _ := strconv.Atoi(os.Getenv(envMaxConcurrentRow))
-	if maxConcurrentRows == 0 {
-		log.Printf("[INFO] No max row concurrency set")
-		maxConcurrentRows = defaultMaxConcurrentRows
-	} else {
-		log.Printf("[INFO] Setting max concurrent rows %d", maxConcurrentRows)
-	}
-	return maxConcurrentRows
 }
 
 func GetMaxMemoryBytes() int64 {
