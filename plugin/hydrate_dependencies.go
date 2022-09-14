8cd7d7aadd384843edaf3d94ac62374f868f7edf
Puskar Basu <puskar@turbot.com>
2022-09-14 19:57:25 +0800

    updates

--- plugin/hydrate_dependencies.go
+++ plugin/hydrate_dependencies.go
@@ -1,10 +1,7 @@
 package plugin
 
 /*
-HydrateDependencies defines the hydrate function dependencies - other hydrate functions
-which must be run first.
-
-This function is deprecated and the functionality is moved into [HydrateConfig].
+Deprecated: Use [HydrateConfig] instead.
 */
 type HydrateDependencies struct {
 	Func    HydrateFunc
