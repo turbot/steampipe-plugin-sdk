389e881c9f7f1fbf8adf3d21208eced6214103fd
misraved <ved@turbot.com>
2022-09-14 20:30:07 +0800

    Update

--- plugin/context.go
+++ plugin/context.go
@@ -9,21 +9,15 @@ import (
 )
 
 /* 
-Logger function extracts the logs from the context. It is useful for debugging in the event of failures.
+Logger retrieves the [hclog.Logger] from the context.
 
-The logs can be visualized by using "STEAMPIPE_LOG=TRACE" command.
+Usage:
 
-Example from [hackernews].
+	plugin.Logger(ctx).Trace("Code execution starts here")
+	plugin.Logger(ctx).Error("hackernews_item.itemList", "query_error", err)
+	plugin.Logger(ctx).Warn("getDomain", "invalid_name", err, "query_response", resp)
+	plugin.Logger(ctx).Info("listGreeting", "number", i)
 
-	func itemList(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-		resp, err := http.Get("https://hacker-news.firebaseio.com/v0/maxitem.json")
-		if err != nil {
-			plugin.Logger(ctx).Error("hackernews_item.itemList", "query_error", err)
-			return nil, err
-		}
-	  ...
-	}
-[hackernews]: https://github.com/turbot/steampipe-plugin-hackernews/blob/d14efdd3f2630f0146e575fe07666eda4e126721/hackernews/table_hackernews_item.go#L32
 */
 func Logger(ctx context.Context) hclog.Logger {
 	return ctx.Value(context_key.Logger).(hclog.Logger)
@@ -41,13 +35,18 @@ func GetMatrixItem(ctx context.Context) map[string]interface{} {
 }
 
 /* 
-IsCancelled is a helper function which returns whether the context has been cancelled.
+A helper function which returns whether the context has been cancelled.
 
-It is instrumental in the event a context has been cancelled due to manual cancellation or the limit has been hit.
+Example from [heroku]:
 
-Example from [hackernews].
+	for _, i := range items {
+		d.StreamListItem(ctx, i)
+		if plugin.IsCancelled(ctx) {
+			return nil, nil
+		}
+	}
 
-[hackernews]: https://github.com/turbot/steampipe-plugin-heroku/blob/a811484d8e29d7478dd9d08adddf0f660563a8ea/heroku/table_heroku_key.go#L58
+[heroku]: https://github.com/turbot/steampipe-plugin-heroku/blob/a811484d8e29d7478dd9d08adddf0f660563a8ea/heroku/table_heroku_key.go#L58
 */
 func IsCancelled(ctx context.Context) bool {
 	return error_helpers.IsContextCancelledError(ctx.Err())
