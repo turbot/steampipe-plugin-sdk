9a45704cc72a4fbd5541107875505f302927ec55
Puskar Basu <puskar@turbot.com>
2022-09-14 19:01:46 +0800

    Update godoc format in hydrate_dependencies.go

--- plugin/hydrate_dependencies.go
+++ plugin/hydrate_dependencies.go
@@ -1,7 +1,11 @@
 package plugin
 
-// HydrateDependencies defines the hydrate function dependencies - other hydrate functions which must be run first
-// Deprecated: used HydrateConfig
+/*
+HydrateDependencies defines the hydrate function dependencies - other hydrate functions
+which must be run first.
+
+This function is deprecated and the functionality is moved into [HydrateConfig].
+*/
 type HydrateDependencies struct {
 	Func    HydrateFunc
 	Depends []HydrateFunc
