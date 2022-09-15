b5babf60788b89a938ac8737eacc60116bc9ab9f
misraved <ved@turbot.com>
2022-09-15 12:27:32 +0800

    Update godoc format in ignore_error_config.go

--- plugin/ignore_error_config.go
+++ plugin/ignore_error_config.go
@@ -8,6 +8,37 @@ import (
 	"github.com/turbot/go-kit/helpers"
 )
 
+/*
+IgnoreConfig defines a set of errors that you want Steampipe to ignore and return an empty row.
+It can be defined in the [GetConfig], [ListConfig] struct at the table level and also at the plugin level.
+
+Usage:
+		// At the table level
+		Get: &plugin.GetConfig{
+			IgnoreConfig: &plugin.IgnoreConfig{
+				ShouldIgnoreErrorFunc: isIgnorableErrorPredicate([]string{"Request_ResourceNotFound", "Invalid object identifier"}),
+			},
+			...
+		},
+
+		List: &plugin.ListConfig{
+			IgnoreConfig: &plugin.IgnoreConfig{
+				ShouldIgnoreErrorFunc: isIgnorableErrorPredicate([]string{"Request_UnsupportedQuery"}),
+			},
+			...
+		},
+
+		// At the plugin level
+		DefaultGetConfig: &plugin.GetConfig{
+			IgnoreConfig: &plugin.IgnoreConfig{
+				ShouldIgnoreErrorFunc: isIgnorableErrorPredicate([]string{"Request_ResourceNotFound"}),
+			},
+		},
+
+Example from [azuread].
+
+[azuread]: https://github.com/turbot/steampipe-plugin-azuread/blob/f4848195931ca4d97a67e930a493f91f63dfe86d/azuread/table_azuread_application.go#L25-L43
+*/
 type IgnoreConfig struct {
 	ShouldIgnoreErrorFunc ErrorPredicateWithContext
 	// deprecated, used ShouldIgnoreErrorFunc
