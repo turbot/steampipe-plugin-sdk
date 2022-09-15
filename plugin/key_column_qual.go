acc926cf3af1b6ddfea74c6e04747f1dcf760f33
judell <jon@jonudell.info>
2022-09-14 17:27:05 -0700

    example for plugin/key_column_qual.go

--- plugin/key_column_qual.go
+++ plugin/key_column_qual.go
@@ -6,7 +6,38 @@ import (
 	"github.com/turbot/go-kit/helpers"
 )
 
-// KeyColumnQuals is a struct representing all quals for a specific column
+/* 
+A struct that represents all [qualifiers] for a column. 
+
+[qualifiers]: https://steampipe.io/docs/develop/writing-plugins#qualifiers
+
+Use it in a [table definition], by way of the [plugin.QueryData] object.
+
+[table definition]: https://steampipe.io/docs/develop/writing-plugins#table-definition
+
+The query writer must specify the qualifiers, in a WHERE or JOIN..ON clause, in order to limit the number of API calls that Steampipe makes to satisfy the query.
+
+Example from [hackernews]:
+
+[hackernews]: https://github.com/turbot/steampipe-plugin-hackernews/blob/bbfbb12751ad43a2ca0ab70901cde6a88e92cf44/hackernews/table_hackernews_user.go#L40
+
+func listUser(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	var item User
+	var id string
+
+	if h.Item != nil {
+		user := h.Item.(*User)
+		id = user.ID
+	} else {
+		quals := d.KeyColumnQuals
+		id = quals["id"].GetStringValue()
+	}
+	...
+}
+
+—
+
+*/
 type KeyColumnQuals struct {
 	Name  string
 	Quals quals.QualSlice
