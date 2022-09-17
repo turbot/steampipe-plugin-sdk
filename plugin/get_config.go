e774a7f013dfcb8c4231aaaabbcbc85e4527d74d
Puskar Basu <puskar@turbot.com>
2022-09-17 14:43:57 +0800

    capitalization

--- plugin/get_config.go
+++ plugin/get_config.go
@@ -11,11 +11,11 @@ import (
 /*
 A GetConfig defines how to get a single row of a table:
 
-  - the [key_columns] that uniquely identify a row.
+  - The [key_columns] that uniquely identify a row.
 
-  - the [error_handling] behaviour.
+  - The [error_handling] behaviour.
 
-  - how many concurrent [HydrateFunc] calls to allow, [when the get call is used as a column hydrate func]: [plugin.GetConfig.MaxConcurrency]
+  - How many concurrent [HydrateFunc] calls to allow, [when the get call is used as a column hydrate func]: [plugin.GetConfig.MaxConcurrency].
 
 A GetConfig with KeyColumns:
 
