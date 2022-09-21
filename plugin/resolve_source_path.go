6e9a3b5682c5f8f57c8f0c9d7641c406058b44d3
Subhajit Mondal <subhajit@turbot.com>
2022-09-21 18:09:22 +0800

    Update globPattern

--- plugin/resolve_source_path.go
+++ plugin/resolve_source_path.go
@@ -4,38 +4,50 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/go-getter"
 	filehelpers "github.com/turbot/go-kit/files"
 )
 
-func ResolveSourcePath(source, tmpDir string) (sourceDir string, globPattern string, err error) {
-	if source == "" {
+func ResolveSourcePath(sourcePath, tmpDir string) (sourceDir string, globPattern string, err error) {
+	if sourcePath == "" {
 		return "", "", fmt.Errorf("source cannot be empty")
 	}
 
-	sanitizedSource, _ := filehelpers.Tildefy(source)
-
-	sourceDir, globPattern, err = filehelpers.PathToRootAndGlob(sanitizedSource)
+	sourceDir, globPattern, err = filehelpers.GlobRoot(sourcePath)
 	if err != nil {
 		return "", "", err
 	}
 
+	// if we managed to resolve the sourceDir, treat this as a local path
 	if sourceDir != "" {
 		return sourceDir, globPattern, nil
 	}
 
 	// create temporary directory to store the go-getter data
 	dest := createTempDirForGet(tmpDir)
-	err = getter.Get(dest, source)
+
+	lastIndex := strings.LastIndex(sourcePath, "//")
+	if lastIndex != -1 && sourcePath[lastIndex-1:lastIndex] != ":" {
+		globPattern = sourcePath[lastIndex+2:]
+		sourcePath = sourcePath[:lastIndex]
+	}
+
+	err = getter.Get(dest, sourcePath)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to get directory specified by the source %s: %s", source, err.Error())
+		return "", "", fmt.Errorf("failed to get directory specified by the source %s: %s", sourcePath, err.Error())
+	}
+
+	if globPattern != "" {
+		globPattern = path.Join(dest, globPattern)
 	}
 
 	return dest, globPattern, nil
 }
 
+// create a uniquely named sub-directory
 func createTempDirForGet(tmpDir string) string {
 	var dest string
 	for {
@@ -45,7 +57,7 @@ func createTempDirForGet(tmpDir string) string {
 			break
 		}
 
-		// Return true if not a duplicate directory
+		// return true if unique
 		if os.IsNotExist(err) {
 			break
 		}
@@ -54,7 +66,7 @@ func createTempDirForGet(tmpDir string) string {
 	return dest
 }
 
-// Get the current timestamp
+// get the current timestamp
 func timestamp() string {
 	return time.Now().UTC().Format(time.RFC3339)
 }
