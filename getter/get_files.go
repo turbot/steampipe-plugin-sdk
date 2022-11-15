f168e6a408ceea82fd1d08c54d3d3bbb723f87dd
kai <kai@turbot.com>
2022-11-15 14:27:27 +0000

    refactor s3 specific code

--- getter/get_files.go
+++ getter/get_files.go
@@ -47,29 +47,17 @@ func GetFiles(sourcePath, tmpDir string) (localSourcePath string, globPattern st
 	// send the path directly to go-getter, and use the destination path as glob pattern for file searching
 	// and also remove the query parameters (parts after ?) from destination path
 
-	// IF THIS IS AN S3 FILENAME
-	// special case S3 code
+	// If there is no glob pattern, source path is a filename - make the glob pattern the full DESTINATION file path
 	if globPattern == "" {
 		parts := strings.Split(remoteSourcePath, string(os.PathSeparator))
+		// extract file name from remoteSourcePath
 		filename := parts[len(parts)-1]
+		// build the dest filename anmd assign to glob
 		dest = path.Join(dest, filename)
 		globPattern = dest
 	} else {
-		// IF THIS IS AN S3 FOLDER
-		// s3::https://my-bucket.s3.us-east-1.amazonaws.com/test_folder//*.ext?aws_profile=test_profile
-		if strings.Contains(remoteSourcePath, "amazonaws.com/") {
-			// add the bucket folder to the end of the dest path
-			sourceSplit := strings.Split(remoteSourcePath, "amazonaws.com/")
-			if len(sourceSplit) > 1 {
-				dest = path.Join(dest, strings.Split(sourceSplit[1], "?")[0])
-			}
-
-			// go-getter supports as extra / at the end of the source path
-			// which will download all data stored in that path
-			if !strings.HasSuffix(remoteSourcePath, "/") {
-				remoteSourcePath += "/"
-			}
-		}
+		// so this is a folder - apply special case s3 handling
+		remoteSourcePath, dest = handleS3FolderPath(remoteSourcePath, dest)
 	}
 
 	// if any query string passed in the URL, it will appear in u.RawQuery
@@ -102,6 +90,40 @@ func GetFiles(sourcePath, tmpDir string) (localSourcePath string, globPattern st
 	}
 
 	return dest, globPattern, nil
+}
+
+func handleS3FolderPath(remoteSourcePath string, dest string) (string, string) {
+	// When querying s3 with go-getter, we specify the folder as part of the url
+	// (as opposed to after a double slash - as it is for github/gitlab etc)
+	// e.g:
+	// 	s3::https://my-bucket.s3.us-east-1.amazonaws.com/test_folder//*.ext?aws_profile=test_profile
+	//
+	// In this case we need to extract the folder path from the url to build the dest path.
+	//
+	// We do this by taking everything after the string "amazonaws.com/"
+	if strings.Contains(remoteSourcePath, "amazonaws.com/") {
+		// add the bucket folder to the end of the dest path
+		sourceSplit := strings.Split(remoteSourcePath, "amazonaws.com/")
+		if len(sourceSplit) > 1 {
+			// extract the folder path, (removing query params, which will be re-added later)
+			folderName := strings.Split(sourceSplit[1], "?")[0]
+			// set dest to the dest folder path
+			dest = path.Join(dest, folderName)
+		}
+
+		// go-getter supports an extra / at the end of the source path
+		// which will download all data stored in that path
+
+		// for example if source path is specified as:
+		// 	s3::https://my-bucket.s3.us-east-1.amazonaws.com//*.ext?aws_profile=test_profile
+		// we need to pass the following to go getter
+		// (query param is re-added later)
+		// 	s3::https://my-bucket.s3.us-east-1.amazonaws.com/?aws_profile=test_profile
+		if !strings.HasSuffix(remoteSourcePath, "/") {
+			remoteSourcePath += "/"
+		}
+	}
+	return remoteSourcePath, dest
 }
 
 func resolveGlobAndSourcePath(sourcePath string) (remoteSourcePath, glob string, urlData *url.URL, err error) {
