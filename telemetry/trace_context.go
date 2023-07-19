28f2f28d809bc108b4a8987f17573592d0537a0f
Binaek Sarkar <binaek@turbot.com>
2023-07-19 23:52:48 +0530

    enable metrics

--- telemetry/trace_context.go
+++ telemetry/trace_context.go
@@ -5,8 +5,15 @@ import (
 	"fmt"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/trace"
 )
+
+// TraceCtx is a struct which contains a span and the associated context
+type TraceCtx struct {
+	Ctx  context.Context
+	Span trace.Span
+}
 
 func GetTracer(service string) trace.Tracer {
 	return otel.GetTracerProvider().Tracer(service)
@@ -15,4 +22,8 @@ func GetTracer(service string) trace.Tracer {
 func StartSpan(baseCtx context.Context, service string, format string, args ...interface{}) (context.Context, trace.Span) {
 	tr := GetTracer(service)
 	return tr.Start(baseCtx, fmt.Sprintf(format, args...))
+}
+
+func GetMeter(name string, opts ...metric.MeterOption) metric.Meter {
+	return otel.GetMeterProvider().Meter(name, opts...)
 }
