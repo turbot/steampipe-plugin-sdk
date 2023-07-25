e0b0122b2e36af0a34d923014b57766bf7645b6d
Binaek Sarkar <binaek@turbot.com>
2023-07-25 17:05:15 +0530

    remove unnecessary export

--- telemetry/trace_context.go
+++ telemetry/trace_context.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -23,8 +22,4 @@ func GetTracer(service string) trace.Tracer {
 func StartSpan(baseCtx context.Context, service string, format string, args ...interface{}) (context.Context, trace.Span) {
 	tr := GetTracer(service)
 	return tr.Start(baseCtx, fmt.Sprintf(format, args...))
-}
-
-func GetMeter(name string, opts ...metric.MeterOption) metric.Meter {
-	return otel.GetMeterProvider().Meter(name, opts...)
 }
