b064c59a0590562699700158a1c9ddc7679440d0
kai <kai@turbot.com>
2023-09-29 18:41:15 +0100

    v5.6.1-dev.1

--- version/version.go
+++ version/version.go
@@ -17,7 +17,7 @@ var version = "5.6.1"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var prerelease = "rc.1"
+var prerelease = "dev.1"
 
 // semVer is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
