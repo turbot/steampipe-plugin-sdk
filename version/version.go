1d50fd5ddc62fac69132241c34c0d281e885576f
kai <kai@turbot.com>
2023-12-07 10:37:00 +0000

    v5.8.0-rc.0

--- version/version.go
+++ version/version.go
@@ -12,12 +12,12 @@ import (
 var ProtocolVersion int64 = 20220201
 
 // Version is the main version number that is being run at the moment.
-var version = "5.7.0"
+var version = "5.8.0"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var prerelease = "alpha.9"
+var prerelease = "rc.0"
 
 // semVer is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
